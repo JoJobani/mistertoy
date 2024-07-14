ad6bc52bae3e9b24230fe2271cd66e3102635e41
JoJobani <hvu2309@gmail.com>
2024-07-14 16:33:02 +0300

    Update server file

--- server.js
+++ server.js
@@ -1,113 +1,51 @@
-import path from 'path';
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-import { toyService } from './services/toy.service.js'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
-
-//App config
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:3000',
-        'http://localhost:3000',
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-    ],
-    credentials: true
-}
-app.use(cors(corsOptions))
 
 // Express Config:
 app.use(express.static('public'))
 app.use(cookieParser())
 app.use(express.json())
 
-// **************** Toys API ****************:
-//Toy list
-app.get('/api/toy', (req, res) => {
-    const { filterBy = {}, sortBy = {} } = req.query
-    toyService.query(filterBy, sortBy)
-        .then((toys) => {
-            res.send(toys)
-        })
-        .catch((err) => {
-            loggerService.error('Cannot get toys', err)
-            res.status(400).send('Cannot get toys')
-        })
-})
+//App config
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: [
+            'http://127.0.0.1:5173',
+            'http://localhost:5173',
 
-// toy READ
-app.get('/api/toy/:toyId', (req, res) => {
-    const { toyId } = req.params
-    toyService.getById(toyId)
-        .then((toy) => {
-            res.send(toy)
-        })
-        .catch((err) => {
-            loggerService.error('Cannot get toy', err)
-            res.status(400).send('Cannot get toy')
-        })
-})
-
-// toy CREATE
-app.post('/api/toy', (req, res) => {
-    const { name, price, labels } = req.body
-    const toy = {
-        name,
-        price: +price,
-        labels
+            'http://127.0.0.1:3030',
+            'http://localhost:3030',
+        ],
+        credentials: true
     }
-    toyService.save(toy)
-        .then((savedtoy) => {
-            res.send(savedtoy)
-        })
-        .catch((err) => {
-            console.log('Cannot save toy', err)
-            res.status(400).send('Cannot save toy')
-        })
+    app.use(cors(corsOptions))
+}
 
-})
+// import { authRoutes } from './api/auth/auth.routes.js'
+// import { userRoutes } from './api/user/user.routes.js'
+import { toyRoutes } from './api/toy/toy.routes.js'
 
-// toy UPDATE
-app.put('/api/toy', (req, res) => {
-    const { name, price, _id, labels } = req.body
-    const toy = {
-        _id,
-        name,
-        price: +price,
-        labels
-    }
-    toyService.save(toy)
-        .then((savedtoy) => {
-            res.send(savedtoy)
-        })
-        .catch((err) => {
-            console.log('Cannot save toy', err)
-            res.status(400).send('Cannot save toy')
-        })
-})
-
-// toy DELETE
-app.delete('/api/toy/:toyId', (req, res) => {
-    const { toyId } = req.params
-    toyService.remove(toyId)
-        .then(() => {
-            res.send('Removed!')
-        })
-        .catch((err) => {
-            console.log('Cannot remove toy', err)
-            res.status(400).send('Cannot remove toy')
-        })
-})
+//Routes
+// app.use('/api/auth', authRoutes)
+// app.use('/api/user', userRoutes)
+app.use('/api/toy', toyRoutes)
 
 //Fallback
 app.get('/**', (req, res) => {
     res.sendFile(path.resolve('public/index.html'))
 })
 
-const port = 3030
+const port = process.env.PORT || 3030
 app.listen(port, () =>
     console.log(`Server listening on port http://127.0.0.1:${port}/`)
 )
